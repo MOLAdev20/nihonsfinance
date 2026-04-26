adeafcb5f0642f65467e59fb03b0a24ba541c812
Sabilul Hikam <billhikam2003@gmail.com>
2026-04-26 17:12:30 +0700

    feature: add crud product

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/admin', 'admin.dashboard')->name('admin.dashboard');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::view('/', 'admin.dashboard')->name('dashboard');
+
+    // Product management routes
+    Route::prefix('product')->name('product.')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('index');
+        Route::post('/', [ProductController::class, 'store'])->name('store');
+        Route::put('/{product}', [ProductController::class, 'update'])->name('update');
+        Route::delete('/{product}', [ProductController::class, 'destroy'])->name('destroy');
+    });
+});
